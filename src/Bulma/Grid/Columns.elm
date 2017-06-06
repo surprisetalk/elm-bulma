2678990e9890459605c536a0f7c3e237e4695b9d
Taylor Sarrafian <surprisetalk@gmail.com>
2017-06-05 22:56:07 -0700

    grid

--- src/Bulma/Grid/Columns.elm
+++ src/Bulma/Grid/Columns.elm
@@ -1,16 +1,11 @@
 
-module Bulma.Grid.Columns exposing ( Size
-                                   , size
-                                   , threeQuarters
-                                   , twoThirds
-                                   , half
-                                   , oneThird
-                                   , oneQuarter
+module Bulma.Grid.Columns exposing ( Columns
+                                   , columns
                                    )
 
 -- IMPORTS ---------------------------------------------------------------------
 
-import Bulma.Helpers exposing ( Devices )
+import Bulma.Helpers exposing ( Helpers, defaultHelpers, node )
 
 import Html exposing ( Html, Attribute )
 import Html.Attributes exposing ( classList )
@@ -22,9 +17,35 @@ import Html.Attributes exposing ( classList )
 (=>) = (,)
 
 
--- MODIFIERS -------------------------------------------------------------------
+-- COLUMN ----------------------------------------------------------------------
+
+type Column msg = Column { helps  : Helpers
+                         , attrs  : List (Attribute msg)
+                         , htmls  : List (Html      msg)
+                         , offset : Size
+                         , sizes  : { mobile  : Size
+                                    , tablet  : Size
+                                    , desktop : Size
+                                    }
+                         }
+
+column : List (Attribute msg) -> List (Html msg) -> Column msg
+column attrs htmls
+  = Column { helps  = defaultHelpers
+           , attrs  = attrs
+           , htmls  = htmls
+           , offset = Auto
+           , sizes  = { mobile  = Auto
+                      , tablet  = Auto
+                      , desktop = Auto
+                      }
+           }
+
+
+-- DEVICE SIZES --
 
 type Size = Auto
+          | Narrow
           | Size01
           | Size02
           | Size03
@@ -43,39 +64,217 @@ type Size = Auto
           | OneThird
           | OneQuarter
 
-auto : Size
-auto = Auto
 
-size : Int -> Size
-size n = case clamp 1 12 n of
-           01 -> Size01
-           02 -> Size02
-           03 -> Size03
-           04 -> Size04
-           05 -> Size05
-           06 -> Size06
-           07 -> Size07
-           08 -> Size08
-           09 -> Size09
-           10 -> Size10
-           11 -> Size11
-           12 -> Size12
-           _  -> Auto
+type Device = All
+            | Mobile
+            | Tablet
+            | Desktop
 
-threeQuarters : Size
-threeQuarters = ThreeQuarters
+setDeviceSize : Device -> Size -> Column msg -> Column msg
+setDeviceSize device size_ (Column ({sizes} as column))
+  = { column
+    | sizes = case device of
+                Mobile  -> { sizes | mobile  = size_ }
+                Tablet  -> { sizes | tablet  = size_ }
+                Desktop -> { sizes | desktop = size_ }
+                All     -> { sizes | mobile  = size_
+                                  , tablet  = size_
+                                  , desktop = size_ }
+    }
 
-twoThirds : Size
-twoThirds = TwoThirds
+size : Maybe Int -> Size
+size n = case n |> Maybe.map (clamp 1 12) of
+           Just 01 -> Size01
+           Just 02 -> Size02
+           Just 03 -> Size03
+           Just 04 -> Size04
+           Just 05 -> Size05
+           Just 06 -> Size06
+           Just 07 -> Size07
+           Just 08 -> Size08
+           Just 09 -> Size09
+           Just 10 -> Size10
+           Just 11 -> Size11
+           Just 12 -> Size12
+           _       -> Auto
 
-half : Size
-half = Half
+setSize : Maybe Int -> Column msg -> Column msg
+setSize = size >> setDeviceSize All
 
-oneThird : Size
-oneThird = OneThird
+size1      : Column msg -> Column msg
+size1      = setDeviceSize All Size01      
+             
+size2      : Column msg -> Column msg
+size2      = setDeviceSize All Size02      
+             
+size3      : Column msg -> Column msg
+size3      = setDeviceSize All Size03      
+             
+size4      : Column msg -> Column msg
+size4      = setDeviceSize All Size04      
+             
+size5      : Column msg -> Column msg
+size5      = setDeviceSize All Size05      
+             
+size6      : Column msg -> Column msg
+size6      = setDeviceSize All Size06      
+             
+size7      : Column msg -> Column msg
+size7      = setDeviceSize All Size07      
+             
+size8      : Column msg -> Column msg
+size8      = setDeviceSize All Size08      
+             
+size9      : Column msg -> Column msg
+size9      = setDeviceSize All Size09      
+             
+size10     : Column msg -> Column msg
+size10     = setDeviceSize All Size10     
+             
+size11     : Column msg -> Column msg
+size11     = setDeviceSize All Size11     
+             
+size12     : Column msg -> Column msg
+size12     = setDeviceSize All Size12     
+             
+sizeAuto   : Column msg -> Column msg
+sizeAuto   = setDeviceSize All Auto   
+             
+sizeNarrow : Column msg -> Column msg
+sizeNarrow = setDeviceSize All Narrow 
+             
 
-oneQuarter : Size
-oneQuarter = OneQuarter
+mobileSize : Maybe Int -> Column msg -> Column msg
+mobileSize = size >> setDeviceSize Mobile
+
+mobileSize01      : Column msg -> Column msg
+mobileSize01      = setDeviceSize Mobile Size01      
+                    
+mobileSize02      : Column msg -> Column msg
+mobileSize02      = setDeviceSize Mobile Size02      
+                    
+mobileSize03      : Column msg -> Column msg
+mobileSize03      = setDeviceSize Mobile Size03      
+                    
+mobileSize04      : Column msg -> Column msg
+mobileSize04      = setDeviceSize Mobile Size04      
+                    
+mobileSize05      : Column msg -> Column msg
+mobileSize05      = setDeviceSize Mobile Size05      
+                    
+mobileSize06      : Column msg -> Column msg
+mobileSize06      = setDeviceSize Mobile Size06      
+                    
+mobileSize07      : Column msg -> Column msg
+mobileSize07      = setDeviceSize Mobile Size07      
+                    
+mobileSize08      : Column msg -> Column msg
+mobileSize08      = setDeviceSize Mobile Size08      
+                    
+mobileSize09      : Column msg -> Column msg
+mobileSize09      = setDeviceSize Mobile Size09      
+                    
+mobileSize10     : Column msg -> Column msg
+mobileSize10     = setDeviceSize Mobile Size10     
+                   
+mobileSize11     : Column msg -> Column msg
+mobileSize11     = setDeviceSize Mobile Size11     
+                   
+mobileSize12     : Column msg -> Column msg
+mobileSize12     = setDeviceSize Mobile Size12     
+                   
+mobileSizeAuto   : Column msg -> Column msg
+mobileSizeAuto   = setDeviceSize Mobile Auto   
+                   
+mobileSizeNarrow : Column msg -> Column msg
+mobileSizeNarrow = setDeviceSize Mobile Narrow 
+
+tabletSize : Maybe Int -> Column msg -> Column msg
+tabletSize = size >> setDeviceSize Tablet
+
+tabletSize01      : Column msg -> Column msg
+tabletSize01      = setDeviceSize Tablet Size01      
+                    
+tabletSize02      : Column msg -> Column msg
+tabletSize02      = setDeviceSize Tablet Size02      
+                    
+tabletSize03      : Column msg -> Column msg
+tabletSize03      = setDeviceSize Tablet Size03      
+                    
+tabletSize04      : Column msg -> Column msg
+tabletSize04      = setDeviceSize Tablet Size04      
+                    
+tabletSize05      : Column msg -> Column msg
+tabletSize05      = setDeviceSize Tablet Size05      
+                    
+tabletSize06      : Column msg -> Column msg
+tabletSize06      = setDeviceSize Tablet Size06      
+                    
+tabletSize07      : Column msg -> Column msg
+tabletSize07      = setDeviceSize Tablet Size07      
+                    
+tabletSize08      : Column msg -> Column msg
+tabletSize08      = setDeviceSize Tablet Size08      
+                    
+tabletSize09      : Column msg -> Column msg
+tabletSize09      = setDeviceSize Tablet Size09      
+                    
+tabletSize10     : Column msg -> Column msg
+tabletSize10     = setDeviceSize Tablet Size10     
+                   
+tabletSize11     : Column msg -> Column msg
+tabletSize11     = setDeviceSize Tablet Size11     
+                   
+tabletSize12     : Column msg -> Column msg
+tabletSize12     = setDeviceSize Tablet Size12     
+                   
+tabletSizeAuto   : Column msg -> Column msg
+tabletSizeAuto   = setDeviceSize Tablet Auto   
+                   
+tabletSizeNarrow : Column msg -> Column msg
+tabletSizeNarrow = setDeviceSize Tablet Narrow 
+
+desktopSize : Maybe Int -> Column msg -> Column msg
+desktopSize = size >> setDeviceSize Desktop
+
+desktopSize01      : Column msg -> Column msg
+desktopSize01      = setDeviceSize Desktop Size01      
+                     
+desktopSize02      : Column msg -> Column msg
+desktopSize02      = setDeviceSize Desktop Size02      
+                     
+desktopSize03      : Column msg -> Column msg
+desktopSize03      = setDeviceSize Desktop Size03      
+                     
+desktopSize04      : Column msg -> Column msg
+desktopSize04      = setDeviceSize Desktop Size04      
+                     
+desktopSize05      : Column msg -> Column msg
+desktopSize05      = setDeviceSize Desktop Size05      
+                     
+desktopSize06      : Column msg -> Column msg
+desktopSize06      = setDeviceSize Desktop Size06      
+                     
+desktopSize07      : Column msg -> Column msg
+desktopSize07      = setDeviceSize Desktop Size07      
+                     
+desktopSize08      : Column msg -> Column msg
+desktopSize08      = setDeviceSize Desktop Size08      
+                     
+desktopSize09      : Column msg -> Column msg
+desktopSize09      = setDeviceSize Desktop Size09      
+                     
+desktopSize10     : Column msg -> Column msg
+desktopSize10     = setDeviceSize Desktop Size10     
+                    
+desktopSize11     : Column msg -> Column msg
+desktopSize11     = setDeviceSize Desktop Size11     
+                    
+desktopSize12     : Column msg -> Column msg
+desktopSize12     = setDeviceSize Desktop Size12     
+                    
+desktopSizeNarrow : Column msg -> Column msg
+desktopSizeNarrow = setDeviceSize Desktop Narrow 
 
 sizeToString : Size -> Maybe String
 sizeToString size
@@ -100,115 +299,158 @@ sizeToString size
       OneQuarter    -> Just "one-quarter"
 
 
--- COLUMN ----------------------------------------------------------------------
+-- OFFSETS --
 
--- TODO: make a nice devices api, with helpful functions to generate options
+setOffset : Size -> Column msg -> Column msg
+setOffset size_ (Column column)
+  = Column { column | offset = size_ }
 
-type Column msg = Column (Html msg)
+offset : Maybe Int -> Column msg -> Column msg
+offset = size >> setOffset
 
-type alias Offset = Size
-
-
-column : Devices Size -> Offset -> List (Attribute msg) -> List (Html msg) -> Column msg
-column {mobile,tablet,desktop} offset attrs htmls
-  = let attrs_ : List (Attribute msg)
-        attrs_ = flip (::) attrs
-               <| classList
-                 [ "column"  => True
-                 , mobile
-                   |> sizeToString
-                   |> Maybe.map ((++) "is-mobile-" >> flip (,) True)
-                   |> Maybe.withDefault ("",False)
-                 , tablet
-                   |> sizeToString
-                   |> Maybe.map ((++) "is-tablet-" >> flip (,) True)
-                   |> Maybe.withDefault ("",False)
-                 , desktop
-                   |> sizeToString
-                   |> Maybe.map ((++) "is-desktop-" >> flip (,) True)
-                   |> Maybe.withDefault ("",False)
-                 , offset
-                   |> sizeToString
-                   |> Maybe.map ((++) "is-offset-" >> flip (,) True)
-                   |> Maybe.withDefault ("",False)
-                 ]
-
-    in div attrs_ htmls
-
--- narrowColumn : Devices Bool -> List (Attribute msg) -> List (Html msg) -> Column msg
--- -- TODO: True for the devices for which it's narrow
+offset1    : Column msg -> Column msg
+offset1    = setOffset Size01    
+             
+offset2    : Column msg -> Column msg
+offset2    = setOffset Size02    
+             
+offset3    : Column msg -> Column msg
+offset3    = setOffset Size03    
+             
+offset4    : Column msg -> Column msg
+offset4    = setOffset Size04    
+             
+offset5    : Column msg -> Column msg
+offset5    = setOffset Size05    
+             
+offset6    : Column msg -> Column msg
+offset6    = setOffset Size06    
+             
+offset7    : Column msg -> Column msg
+offset7    = setOffset Size07    
+             
+offset8    : Column msg -> Column msg
+offset8    = setOffset Size08    
+             
+offset9    : Column msg -> Column msg
+offset9    = setOffset Size09    
+             
+offset10   : Column msg -> Column msg
+offset10   = setOffset Size10   
+             
+offset11   : Column msg -> Column msg
+offset11   = setOffset Size11   
+             
+offset12   : Column msg -> Column msg
+offset12   = setOffset Size12   
+             
+offsetAuto : Column msg -> Column msg
+offsetAuto = setOffset Auto 
 
 
 -- COLUMNS ---------------------------------------------------------------------
 
-type Gaps = Gapped
-          | Gapless
+type Columns msg = Columns { helps     : Helpers
+                           , attrs     : List (Attribute msg)
+                           , cols      : List (Column    msg)
+                           , multiline : Bool
+                           , gapless   : Bool
+                           , display   : Display
+                           }
 
-gapped : Gaps
-gapped = Gapped
+columns : List (Attribute msg) -> List (Column msg) -> Columns msg
+columns attrs cols
+  = Columns { helps     = defaultHelpers
+            , attrs     = attrs
+            , cols      = cols
+            , multiline = False
+            , gapless   = False
+            , display   = TabletAndBeyond
+            }
 
-gapless : Gaps
-gapless = Gapless
+toColumn : List (Attribute msg) -> Columns msg -> Column msg
+toColumn attrs = column attrs << singleton << toHtml
+
+
+-- MULTILINE --
+
+singleline : Columns msg -> Columns msg
+singleline (Columns columns)
+  = Columns { columns | multiline = False }
+
+multiline : Columns msg -> Columns msg
+multiline (Columns columns)
+  = Columns { columns | multiline = True }
+
+
+-- GAPS --
+
+gapped : Columns msg -> Columns msg
+gapped (Columns columns)
+  = Columns { columns | gapless = False }
+
+gapless : Columns msg -> Columns msg
+gapless (Columns columns)
+  = Columns { columns | gapless = True }
+
+
+-- DISPLAY --
 
 type Display = MobileAndBeyond
              | TabletAndBeyond
              | DesktopAndBeyond
 
-multilineColumns : Gaps -> Display -> List (Attribute msg) -> List (Column msg) -> Html msg
-multilineColumns gaps display attrs cols
-  = let attrs_ : List (Attribute msg)
-        attrs_ = flip (::) attrs
-               <| classList
-                 [ "columns"      => True
-                 , "is-multiline" => True
-                 , "is-gapless"   => (gaps == Gapless)
-                 , case display of
-                     MobileAndBeyond  -> "is-mobile"  => True
-                     TabletAndBeyond  -> ""           => False
-                     DesktopAndBeyond -> "is-desktop" => True
-                 ]
+mobileAndBeyond : Columns msg -> Columns msg
+mobileAndBeyond (Columns columns)
+  = Columns { columns | display = MobileAndBeyond }
 
-        cols_ : List (Html msg)
-        cols_ = cols |> List.map (\(Column html) -> html)
-                 
-    in div attrs_ cols_
+tabletAndBeyond : Columns msg -> Columns msg
+tabletAndBeyond (Columns columns)
+  = Columns { columns | display = TabletAndBeyond }
+                  
+desktopAndBeyond : Columns msg -> Columns msg
+desktopAndBeyond (Columns columns)
+  = Columns { columns | display = DesktopAndBeyond }
 
-columns : Gaps -> Display -> List (Attribute msg) -> List (Column msg) -> Html msg
-columns gaps display attrs cols
-  = let attrs_ : List (Attribute msg)
-        attrs_ = flip (::) attrs
-               <| classList
-                 [ "columns"      => True
-                 , "is-gapless"   => (gaps == Gapless)
-                 , case display of
-                     MobileAndBeyond  -> "is-mobile"  => True
-                     TabletAndBeyond  -> ""           => False
-                     DesktopAndBeyond -> "is-desktop" => True
-                 ]
 
-        cols_ : List (Html msg)
-        cols_ = cols |> List.map (\(Column html) -> html)
-                 
-    in div attrs_ cols_
+-- HTML ------------------------------------------------------------------------
 
--- nestColumns : Gaps -> Display -> List (Attribute msg) -> List (Column msg) -> Column msg
--- nestColumns gaps display attrs cols
---   = let attrs_ : List (Attribute msg)
---         attrs_ = flip (::) attrs
---                <| classList
---                  [ "columns"      => True
---                  , "is-gapless"   => (gaps == Gapless)
---                  , case display of
---                      MobileAndBeyond  -> "is-mobile"  => True
---                      TabletAndBeyond  -> ""           => False
---                      DesktopAndBeyond -> "is-desktop" => True
---                  ]
+toHtml : Columns msg -> Html msg
+toHtml (Columns {helps,attrs,cols,multiline,gapped,display})
+  = let toHtml_ : Column msg -> Html msg
+        toHtml_ (Column {helps,attrs,htmls,sizes,offset})
+          = let classes : List String
+                classes = flip (::) "column"
+                        <| Maybe_.values
+                        <| [ sizes.mobile  |> sizeToString |> Maybe.map (\s -> "is-" ++ s ++ "-mobile" )
+                          , sizes.tablet  |> sizeToString |> Maybe.map (\s -> "is-" ++ s ++ "-tablet" )
+                          , sizes.desktop |> sizeToString |> Maybe.map (\s -> "is-" ++ s ++ "-desktop")
+                          , offset        |> sizeToString |> Maybe.map ((++) "is-offset-")
+                          ]
+        
+            in node helps "div" classes attrs htmls
 
---         cols_ : List (Html msg)
---         cols_ = cols |> List.map (\(Column html) -> html)
-                 
---     in div attrs_ cols_
+        classes : List String
+        classes = [ "columns"
+                  , if       multiline
+                    then "is-multiline"
+                    else ""
+                  , if       gapped
+                    then "is-gapped"
+                    else ""
+                  , case display of
+                      MobileAndBeyond  -> "is-mobile"
+                      TabletAndBeyond  -> ""
+                      DesktopAndBeyond -> "is-tablet"
+                  ]
+        
+    in node helps "div" classes attrs
+     <| map toHtml_
+     <| cols
+                
 
--- TODO: nestMultilineColumns
+-- HELPERS ---------------------------------------------------------------------
 
+setHelpers : Helpers -> Columns msg -> Columns msg
+setHelpers helps_ (Columns columns) = { columns | helps = helps_ }
 
--- src/Bulma/Grid/Tiles.elm
+++ src/Bulma/Grid/Tiles.elm
@@ -1,5 +1,5 @@
 
-module Bulma.Grid.Tiles exposing ( Size
+module Bulma.Grid.Tiles exposing ( Width
                                  , size
                                  )
 
@@ -17,59 +17,196 @@ import Html.Attributes exposing ( classList )
 (=>) = (,)
 
 
--- MODIFIERS -------------------------------------------------------------------
+-- TILE ------------------------------------------------------------------------
 
-type Size = Auto
-          | Size01
-          | Size02
-          | Size03
-          | Size04
-          | Size05
-          | Size06
-          | Size07
-          | Size08
-          | Size09
-          | Size10
-          | Size11
-          | Size12
+type Tile msg = Tile { helps : Helpers
+                     , attrs : List (Attribute msg)
+                     , htmls : List (Html msg)
+                     }
 
-auto : Size
-auto = Auto
+tile : List (Attribute msg) -> List (Html msg) -> Tile msg
 
-size : Int -> Size
-size n = case clamp 1 12 n of
-           01 -> Size01
-           02 -> Size02
-           03 -> Size03
-           04 -> Size04
-           05 -> Size05
-           06 -> Size06
-           07 -> Size07
-           08 -> Size08
-           09 -> Size09
-           10 -> Size10
-           11 -> Size11
-           12 -> Size12
-           _  -> Auto
+-- tileWithHelpers : Helpers -> List (Attribute msg) -> List (Html msg) -> Tile msg
+
+toTile : Box msg -> Tile msg
+-- TODO: move to box?
+
+toTile : Notification msg -> Tile msg
+-- TODO: move to notification?
+
+toTile : Card msg -> Tile msg
+-- TODO: move to card?
+
+-- TODO: more tile types
 
 
 -- TILES -----------------------------------------------------------------------
 
--- TODO: we need two layers
--- TODO:   or we can do some fancy trickery that automagically wraps...
+type Content msg = Ancestor                 (List (Tiles msg))
+                 | Parent Width Orientation (List (Tile  msg))
 
-type Tile msg = Tile (Html msg)
+type Tiles msg = Tiles { helps   : Helpers
+                       , attrs   : List (Attribute msg)
+                       , content : Content
+                       }
   
-tiles : Orientation -> List (Tile msg) -> Tile msg
+easyTiles : List (Maybe (List (Maybe (Tile msg)))) -> Tiles msg
 
-tile : Orientation -> Size -> List (Attribute msg) -> List (Html msg) -> Tile msg
-tile ori size attrs htmls
+-- easyTiles : List (Bool, Maybe Int, List (Tile msg)) -> Tiles msg
+            
+tiles   : List (Attribute msg) -> List (Tile msg) -> Tiles msg
 
--- TODO: we need something like Tile.fromElement, Tile.fromComponent etc.
--- TODO:   we could do tileBox, tileContent, etc.
+tiles1  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles2  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles3  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles4  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles5  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles6  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles7  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles8  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles9  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles10 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles11 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+tiles12 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles   : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles1  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles2  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles3  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles4  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles5  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles6  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles7  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles8  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles9  : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles10 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles11 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+verticalTiles12 : List (Attribute msg) -> List (Tile msg) -> Tiles msg
+
+nestTiles : List (Tiles msg) -> Tiles msg
+
+
+-- -- ORIENTATION --
+
+-- horizontal : Tiles msg -> Tiles msg
+-- horizontal (Tiles tiles)
+--   = { tiles | vertical = True }
+
+-- vertical : Tiles msg -> Tiles msg
+-- vertical (Tiles tiles)
+--   = { tiles | vertical = False }
+
+
+-- -- WIDTH --
+
+-- type Width = Auto
+--            | Width01
+--            | Width02
+--            | Width03
+--            | Width04
+--            | Width05
+--            | Width06
+--            | Width07
+--            | Width08
+--            | Width09
+--            | Width10
+--            | Width11
+--            | Width12
+
+-- setWidth : Int -> Tiles msg -> Tiles msg
+-- setWidth n (Tiles tiles)
+--   = let width_ : Width
+--         width_ = case clamp 0 12 n of
+--                    00 -> Auto
+--                    01 -> Width01
+--                    02 -> Width02
+--                    03 -> Width03
+--                    04 -> Width04
+--                    05 -> Width05
+--                    06 -> Width06
+--                    07 -> Width07
+--                    08 -> Width08
+--                    09 -> Width09
+--                    10 -> Width10
+--                    11 -> Width11
+--                    12 -> Width12
+
+--     in Tiles { tiles | width = width_ }
+
+-- widthAuto : Tiles msg -> Tiles msg
+-- widthAuto = setWidth 0
+
+-- width1  : Tiles msg -> Tiles msg
+-- width1  = setWidth 1
+
+-- width2  : Tiles msg -> Tiles msg
+-- width2  = setWidth 2
+
+-- width3  : Tiles msg -> Tiles msg
+-- width3  = setWidth 3
+
+-- width4  : Tiles msg -> Tiles msg
+-- width4  = setWidth 4
+
+-- width5  : Tiles msg -> Tiles msg
+-- width5  = setWidth 5
+
+-- width6  : Tiles msg -> Tiles msg
+-- width6  = setWidth 6
+
+-- width7  : Tiles msg -> Tiles msg
+-- width7  = setWidth 7
+
+-- width8  : Tiles msg -> Tiles msg
+-- width8  = setWidth 8
+
+-- width9  : Tiles msg -> Tiles msg
+-- width9  = setWidth 9
+
+-- width10 : Tiles msg -> Tiles msg
+-- width10 = setWidth 10
+
+-- width11 : Tiles msg -> Tiles msg
+-- width11 = setWidth 11
+
+-- width12 : Tiles msg -> Tiles msg
+-- width12 = setWidth 12
+
 
 
 -- TRANSFORMS ------------------------------------------------------------------
 
-toHtml -> Orientation -> List (Tile msg) -> Html msg
+toHtml : Tiles msg -> Html msg
+toHtml (Tiles {helps,attrs,content})
+  = let parents : List (Html msg)
+        parents = 
 
+    in node helps "div" [ "tile is-ancestor" ] attrs parents
+
+-- HELPERS ---------------------------------------------------------------------
+
+setHelpers : Helpers -> Tiles msg -> Tiles msg
+setHelpers helps_ (Tiles tiles) = { tiles | helps = helps_ }
