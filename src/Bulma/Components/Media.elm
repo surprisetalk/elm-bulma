edcf8eb7237c672ec7fb566f9b400d1e6757dd95
Taylor Sarrafian <surprisetalk@gmail.com>
2017-06-22 19:31:18 -0700

    media

--- src/Bulma/Components/Media.elm
+++ src/Bulma/Components/Media.elm
@@ -1,9 +1,28 @@
 
 module Bulma.Components.Media exposing ( Media
                                        , media
+                                       , left
+                                       , right
+                                       , toSection
                                        , toHtml
+                                       , addClass
                                        , setHelpers
                                        )
+
+-- DOCS ------------------------------------------------------------------------
+
+{-| TODO 
+
+@docs Media
+@docs media
+
+@docs left, right
+
+@docs toSection
+
+@docs toHtml, addClass, setHelpers
+
+-}
 
 -- IMPORTS ---------------------------------------------------------------------
 
@@ -11,41 +30,60 @@ import Helpers exposing (..)
 import Bulma.Entity as Entity exposing (..)
 import Bulma.Helpers exposing ( Helpers )
 
-import Bulma.Components.Media.Section as Section exposing ( Section )
-
-import Pointless exposing (..)
+import Bulma.Components.Media.Section as Section exposing ( Section, section )
 
 import Html exposing ( Html, Attribute, text )
+
+import List exposing ( concat )
 
 
 -- MEDIA -----------------------------------------------------------------------
 
+{-| TODO
+-}
 type alias Sections msg = List (Section msg)
 
-type alias Media msg = Entity () (Maybe (Sections msg), Sections msg, Maybe (Sections msg)) msg
+{-| TODO
+-}
+type alias Media msg = Entity () { left : Maybe (Sections msg), content : Sections msg, right : Maybe (Sections msg) } msg
 
+{-| TODO
+-}
 media : Attrs msg -> Sections msg -> Media msg
-media = entity "article" [ "media" ] ()
+media attrs content
+  = entity "article" [ "media" ] () attrs
+    { left    = Nothing
+    , content = content
+    , right   = Nothing
+    }
 
+{-| TODO
+-}
 left : Sections msg -> Media msg -> Media msg
-left left_ = mapBody <| \(_,content,right) -> ( Just left_, content, right )
+left left_ = mapBody <| \body -> { body | left = Just left_ }
 
+{-| TODO
+-}
 right : Sections msg -> Media msg -> Media msg
-right right_ = mapBody <| \(left,content,_) -> ( left, content, Just right_ )
+right right_ = mapBody <| \body -> { body | right = Just right_ }
 
-toSection : Media msg -> Section msg
+{-| TODO
+-}
+toSection : Attrs msg -> Media msg -> Section msg
+toSection attrs = toHtml >> ls >> section attrs
 
 -- HTML ------------------------------------------------------------------------
 
 {-| TODO
 -}
-toHtml : Section msg -> Html msg
+toHtml : Media msg -> Html msg
 toHtml = Entity.toHtml (y []) (y [])
-       <| \(left,content,right) ->
-         mvalues
-         [      left    |> Maybe.map (Section.addClass "media-left"    >> Section.toHtml)
-         , Just content |> Maybe.map (Section.addClass "media-content" |> Section.toHtml)
-         ,      right   |> Maybe.map (Section.addClass "media-right"   >> Section.toHtml)
+       <| \{left,content,right} ->
+         concat
+       <| mvalues
+         [      left    |> Maybe.map (List.map (Section.addClass "media-left"    >> Section.toHtml))
+         , Just content |> Maybe.map (List.map (Section.addClass "media-content" >> Section.toHtml))
+         ,      right   |> Maybe.map (List.map (Section.addClass "media-right"   >> Section.toHtml))
          ]
 
 {-| TODO
